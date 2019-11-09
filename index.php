442e66d895c344683c5b622e2f90bf8b3082ed90
bum3144 <bum3144@gmail.com>
2019-11-09 20:12:51 +0800

    php test index.php

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
     <title>PHP example 01</title>
   </head>
   <body>
-
+    <h1>WEB</h1>
+    <ol>
+      <li>HTML</li>
+    </ol>
+    <a href="create.php">create</a>
+    <h2>Welcome</h2>
+    Abcdefghijklmnopqrstl
   </body>
 </html>
