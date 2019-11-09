ad75a056bc65921ab747a0a1361d548d1927b561
bum3144 <bum3144@gmail.com>
2019-11-09 20:13:14 +0800

    php test create.php

---
+++ create.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP example 01</title>
+  </head>
+  <body>
+    <h1>WEB</h1>
+    <ol>
+      <li>HTML</li>
+    </ol>
+    <a href="create.php">create</a>
+    <h2>Welcome</h2>
+    <form action="process_create.php" method="post">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p><textarea name="description" placeholder="description"></textarea></p>
+      <p><input type="submit"></p>
+    </form>
+  </body>
+</html>
